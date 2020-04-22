d3b5316f4ba5c0ab285f8d5b40d25742ed737d6f
porcuon19 <porcuon19@gmail.com>
2020-04-22 16:49:08 +0600

    add send review fragment

---
+++ app/src/main/java/kz/porcuon/recipy/send_review/SendReviewFragment.kt
@@ -0,0 +1,8 @@
+package kz.porcuon.recipy.send_review
+
+import kz.porcuon.recipy.BaseBottomSheetFragment
+import kz.porcuon.recipy.R
+
+class SendReviewFragment : BaseBottomSheetFragment() {
+    override fun getLayoutId(): Int = R.layout.fragment_send_review
+}
